7cd181f187c64f178225b66b2b32796a1f110d98
Dylan Rojas <105813600+DylanRX7xD@users.noreply.github.com>
2024-09-12 08:57:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+print( 'exemplo nome da empresa \n')
+
+print( 'cadastro')
+print(' lista')
+print(' ativar')
+print( 'sair da aplicação')
+
+opcao_escolhido = input('escolha uma opcao')
+print ('voce escolheu a opcao: ', opcao_escolhido)
